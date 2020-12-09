bf7395b31004e79eed6fc829f7a29fa37359fda5
Ahmed <ahmed@someone.com>
2020-12-09 04:34:57 -0800

    initial_2d_walk

---
+++ tranform_latlng_into_2dplane.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+seaborn.set()
+
+def find_xy(df):
+    R = 6371
+    x = R * np.cos(df['Latitude']) * np.cos(df['Longitude'])
+    y = R * np.cos(df['Latitude']) * np.sin(df['Longitude'])
+    df['x'] = x
+    df['y'] = y
+    return df
+df = pd.read_csv('latlng.csv')
+df = df.drop(columns=['Date','Altitude'])
+df = df.apply(find_xy,axis=1)
+plt.plot(df['x'],df['y'])
+#plt.savefig('2dwalk.png')
+plt.show()
+#print(df)
